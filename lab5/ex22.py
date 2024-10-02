493629fc88e1ff6fdffc48156d80dbae8137af41
DarkCrystal111 <korolev1122005@gmail.com>
2024-10-02 22:04:38 +0400

    update

--- lab5/ex22.py
+++ lab5/ex22.py
@@ -1,7 +1,9 @@
 from math import ceil
 y = 40
 count = 0
+total = 0
 while count < 10:
-    y += y/100*5
+    y = y + y/100*5
+    total += y
     count += 1
-print("В книге", ceil(y), "страниц")
+print("В книге", ceil(total), "страниц")
