14069a919b35e60fbc38c9d2208ab80d9eeba453
DarkCrystal111 <korolev1122005@gmail.com>
2024-11-20 13:58:19 +0400

    fix

--- lab7/ex28.py
+++ lab7/ex28.py
@@ -2,6 +2,6 @@ n = int(input("Введите число n: "))
 x = int(input("Введите число x: "))
 val = 0
 for k in range(1, n+1):
-    for m in range(1, k):
+    for m in range(k, n+1):
         val += (x+k)/m
 print(val)
